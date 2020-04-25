2db519a9e56f46a142d9cea76331aeeb74285481
Ildar Bikmamatov <ildar@bayrell.org>
2020-04-25 18:43:30 +0100

    add files

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT=$(readlink -f $0)
+SCRIPT_PATH=`dirname $SCRIPT`
+BASE_PATH=`dirname $SCRIPT_PATH`
+
+RETVAL=0
+TAG=`date '+%Y%m%d_%H%M%S'`
+
+case "$1" in
+	
+	docker)
+		docker build ./ -t bayrell/alpine_php_fpm:$TAG --file Dockerfile
+		docker tag bayrell/alpine_php_fpm:$TAG bayrell/alpine_php_fpm:latest
+	;;
+	
+	*)
+		echo "Usage: $0 {docker}"
+		RETVAL=1
+
+esac
+
+exit $RETVAL
+
