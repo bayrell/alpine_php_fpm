06153f6063eb734e98d70ad572f4ba75b411751a
Ildar Bikmamatov <ildar@bayrell.org>
2020-04-27 09:10:26 +0100

    add 99-default

--- files/root/run.d/10-php.nginx.sh
+++ files/root/run.d/10-php.nginx.sh
@@ -1,8 +1,8 @@
 
 if [ ! -z $ANY_PHP ] && [ "$ANY_PHP" = "1" ]; then
-	sed -i 's|%REWRITE_PHP%|try_files $uri $uri/ /index.php?$args;|g' /etc/nginx/sites-enabled/app.conf
-	sed -i 's|%LOCATION_PHP%|location ~ \\.php$|g' /etc/nginx/sites-enabled/app.conf
+	sed -i 's|%REWRITE_PHP%|try_files $uri $uri/ /index.php?$args;|g' /etc/nginx/sites-enabled/99-default.conf
+	sed -i 's|%LOCATION_PHP%|location ~ \\.php$|g' /etc/nginx/sites-enabled/99-default.conf
 else
-	sed -i 's|%REWRITE_PHP%|rewrite ^/. /index.php last;|g' /etc/nginx/sites-enabled/app.conf
-	sed -i 's|%LOCATION_PHP%|location /index.php|g' /etc/nginx/sites-enabled/app.conf
+	sed -i 's|%REWRITE_PHP%|rewrite ^/. /index.php last;|g' /etc/nginx/sites-enabled/99-default.conf
+	sed -i 's|%LOCATION_PHP%|location /index.php|g' /etc/nginx/sites-enabled/99-default.conf
 fi
