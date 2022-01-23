fc44648c4b47a7c431d9d3f2bc5ca37f3f93ae5a
Ildar Bikmamatov <ildar@bayrell.org>
2022-01-23 09:42:14 +0000

    www user id fix

--- files/root/run.d/50-www.user.sh
+++ files/root/run.d/50-www.user.sh
@@ -1,4 +1,4 @@
 if [ ! -z $WWW_UID ] && [ ! -z $WWW_GID ]; then
-	sed -i "s|:33:33:|:$WWW_UID:$WWW_GID:|g" /etc/passwd
-	sed -i "s|:33:|:$WWW_GID:|g" /etc/group
+	sed -i "s|:800:800:|:$WWW_UID:$WWW_GID:|g" /etc/passwd
+	sed -i "s|:800:|:$WWW_GID:|g" /etc/group
 fi
