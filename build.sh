b018ad4352961a22a6e351052a4eab4ebcc81b64
Ildar Bikmamatov <ildar@bayrell.org>
2022-06-11 12:16:49 +0000

    rm arm32v7

--- build.sh
+++ build.sh
@@ -26,48 +26,37 @@ case "$1" in
 			--file Dockerfile --build-arg ARCH=arm64v8
 	;;
 	
-	arm32v7)
-		docker build ./ -t bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v7 \
-			--file Dockerfile --build-arg ARCH=arm32v7
-	;;
-	
 	manifest)
 		rm -rf ~/.docker/manifests/docker.io_bayrell_$IMAGE_NAME-*
 		
 		docker tag bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64 bayrell/$IMAGE_NAME:$VERSION-amd64
 		docker tag bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8 bayrell/$IMAGE_NAME:$VERSION-arm64v8
-		docker tag bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v7 bayrell/$IMAGE_NAME:$VERSION-arm32v7
 		
 		docker push bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64
 		docker push bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8
-		docker push bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v7
 		
 		docker push bayrell/$IMAGE_NAME:$VERSION-amd64
 		docker push bayrell/$IMAGE_NAME:$VERSION-arm64v8
-		docker push bayrell/$IMAGE_NAME:$VERSION-arm32v7
 		
 		docker manifest create bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION \
 			--amend bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64 \
-			--amend bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8 \
-			--amend bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v7
+			--amend bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8
 		docker manifest push bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION
 		
 		docker manifest create bayrell/$IMAGE_NAME:$VERSION \
 			--amend bayrell/$IMAGE_NAME:$VERSION-amd64 \
-			--amend bayrell/$IMAGE_NAME:$VERSION-arm64v8 \
-			--amend bayrell/$IMAGE_NAME:$VERSION-arm32v7
+			--amend bayrell/$IMAGE_NAME:$VERSION-arm64v8
 		docker manifest push bayrell/$IMAGE_NAME:$VERSION
 	;;
 	
 	all)
 		$0 amd64
 		$0 arm64v8
-		$0 arm32v7
 		$0 manifest
 	;;
 	
 	*)
-		echo "Usage: $0 {amd64|arm64v8|arm32v7|manifest|all|test}"
+		echo "Usage: $0 {amd64|arm64v8|manifest|all|test}"
 		RETVAL=1
 
 esac
