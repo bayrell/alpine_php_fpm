ef59c5c352a234d309e84034d3b742bb18cfb91a
Ildar Bikmamatov <ildar@bayrell.org>
2024-02-24 19:01:17 +0600

    update Asia/Almaty

--- build.sh
+++ build.sh
@@ -6,60 +6,102 @@ BASE_PATH=`dirname $SCRIPT_PATH`
 
 RETVAL=0
 VERSION=8.0
-SUBVERSION=2
-IMAGE_NAME="alpine_php_fpm"
+SUBVERSION=3
+IMAGE="alpine_php_fpm"
 TAG=`date '+%Y%m%d_%H%M%S'`
 
 case "$1" in
 	
 	test)
-		docker build ./ -t bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-$TAG --file Dockerfile
+		docker build ./ -t bayrell/$IMAGE:$VERSION-$SUBVERSION-$TAG --file Dockerfile
 	;;
 	
 	amd64)
 		export DOCKER_DEFAULT_PLATFORM=linux/amd64
-		docker build ./ -t bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64 \
+		docker build ./ -t bayrell/$IMAGE:$VERSION-$SUBVERSION-amd64 \
 			--file Dockerfile --build-arg ARCH=amd64
 	;;
 	
 	arm64v8)
 		export DOCKER_DEFAULT_PLATFORM=linux/arm64/v8
-		docker build ./ -t bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8 \
+		docker build ./ -t bayrell/$IMAGE:$VERSION-$SUBVERSION-arm64v8 \
 			--file Dockerfile --build-arg ARCH=arm64v8
 	;;
 	
 	arm32v7)
 		export DOCKER_DEFAULT_PLATFORM=linux/arm/v7
-		docker build ./ -t bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v7 \
+		docker build ./ -t bayrell/$IMAGE:$VERSION-$SUBVERSION-arm32v7 \
 			--file Dockerfile --build-arg ARCH=arm32v7
 	;;
 	
 	manifest)
 		rm -rf ~/.docker/manifests/docker.io_bayrell_$IMAGE_NAME-*
 		
-		docker tag bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64 bayrell/$IMAGE_NAME:$VERSION-amd64
-		docker tag bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8 bayrell/$IMAGE_NAME:$VERSION-arm64v8
-		docker tag bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v7 bayrell/$IMAGE_NAME:$VERSION-arm32v7
+		docker tag bayrell/$IMAGE:$VERSION-$SUBVERSION-amd64 bayrell/$IMAGE:$VERSION-amd64
+		docker tag bayrell/$IMAGE:$VERSION-$SUBVERSION-arm64v8 bayrell/$IMAGE:$VERSION-arm64v8
+		docker tag bayrell/$IMAGE:$VERSION-$SUBVERSION-arm32v7 bayrell/$IMAGE:$VERSION-arm32v7
 		
-		docker push bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64
-		docker push bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8
-		docker push bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v7
+		docker push bayrell/$IMAGE:$VERSION-$SUBVERSION-amd64
+		docker push bayrell/$IMAGE:$VERSION-$SUBVERSION-arm64v8
+		docker push bayrell/$IMAGE:$VERSION-$SUBVERSION-arm32v7
 		
-		docker push bayrell/$IMAGE_NAME:$VERSION-amd64
-		docker push bayrell/$IMAGE_NAME:$VERSION-arm64v8
-		docker push bayrell/$IMAGE_NAME:$VERSION-arm32v7
+		docker push bayrell/$IMAGE:$VERSION-amd64
+		docker push bayrell/$IMAGE:$VERSION-arm64v8
+		docker push bayrell/$IMAGE:$VERSION-arm32v7
 		
-		docker manifest create bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION \
-			--amend bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64 \
-			--amend bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8 \
-			--amend bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v7
-		docker manifest push bayrell/$IMAGE_NAME:$VERSION-$SUBVERSION
+		docker manifest create bayrell/$IMAGE:$VERSION-$SUBVERSION \
+			--amend bayrell/$IMAGE:$VERSION-$SUBVERSION-amd64 \
+			--amend bayrell/$IMAGE:$VERSION-$SUBVERSION-arm64v8 \
+			--amend bayrell/$IMAGE:$VERSION-$SUBVERSION-arm32v7
+		docker manifest push bayrell/$IMAGE:$VERSION-$SUBVERSION
 		
-		docker manifest create bayrell/$IMAGE_NAME:$VERSION \
-			--amend bayrell/$IMAGE_NAME:$VERSION-amd64 \
-			--amend bayrell/$IMAGE_NAME:$VERSION-arm64v8 \
-			--amend bayrell/$IMAGE_NAME:$VERSION-arm32v7
-		docker manifest push bayrell/$IMAGE_NAME:$VERSION
+		docker manifest create bayrell/$IMAGE:$VERSION \
+			--amend bayrell/$IMAGE:$VERSION-amd64 \
+			--amend bayrell/$IMAGE:$VERSION-arm64v8 \
+			--amend bayrell/$IMAGE:$VERSION-arm32v7
+		docker manifest push bayrell/$IMAGE:$VERSION
+	;;
+	
+	upload-github)
+		docker tag bayrell/$IMAGE:$VERSION-$SUBVERSION-arm64v8 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION-arm64v8
+		
+		docker tag bayrell/$IMAGE:$VERSION-$SUBVERSION-arm32v7 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION-arm32v7
+		
+		docker tag bayrell/$IMAGE:$VERSION-$SUBVERSION-amd64 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION-amd64
+		
+		docker push ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION-amd64
+		docker push ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION-arm32v7
+		docker push ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION-arm64v8
+		
+		docker manifest create --amend \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION-amd64 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION-arm32v7 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION-arm64v8
+		docker manifest push --purge ghcr.io/bayrell-os/$IMAGE:$VERSION-$SUBVERSION
+		
+		docker tag bayrell/$IMAGE:$VERSION-arm64v8 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-arm64v8
+		
+		docker tag bayrell/$IMAGE:$VERSION-arm32v7 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-arm32v7
+		
+		docker tag bayrell/$IMAGE:$VERSION-amd64 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-amd64
+		
+		docker push ghcr.io/bayrell-os/$IMAGE:$VERSION-amd64
+		docker push ghcr.io/bayrell-os/$IMAGE:$VERSION-arm32v7
+		docker push ghcr.io/bayrell-os/$IMAGE:$VERSION-arm64v8
+		
+		docker manifest create --amend \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-amd64 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-arm32v7 \
+			ghcr.io/bayrell-os/$IMAGE:$VERSION-arm64v8
+		docker manifest push --purge ghcr.io/bayrell-os/$IMAGE:$VERSION
 	;;
 	
 	all)
